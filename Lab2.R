eb768256de073df742d924aeb0c4e8c891b33ef8
snauman4 <snauman4@colostate.edu>
2025-02-20 15:28:35 -0700

    final commit

---
+++ Lab2.R
@@ -0,0 +1,12 @@
+---
+title: "Lab 2: Minnesota Tree Growth"
+author: "Samantha Nauman"
+date: today
+format: html
+---
+
+library(dplyr)
+tree_dat <- read.csv("data/tree_dat.csv")
+glimpse("tree_dat")
+str("tree_dat")
+
